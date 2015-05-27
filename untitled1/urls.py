7f658a99c67d9dc812f76322c1d9d9de811ca14e
Sebastian Wozny <arpheno@gmail.com>
2015-05-27 02:25:30 +0200

    Added example for ajax functionality

--- untitled1/urls.py
+++ untitled1/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
+from ajax.views import myFunction
 from reversefk.views import ReverseFK
 
 urlpatterns = patterns('',
@@ -7,6 +9,8 @@ urlpatterns = patterns('',
     # url(r'^$', 'untitled1.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', ReverseFK.as_view()),
+    url(r'^rfk/?$', ReverseFK.as_view()),
+    url(r'^ajax/?$', TemplateView.as_view(template_name="ajax/base.html")),
+    url(r'^myFunction/?$', myFunction),
     url(r'^admin/', include(admin.site.urls)),
 )
